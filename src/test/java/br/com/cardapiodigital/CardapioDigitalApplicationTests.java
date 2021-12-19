15d35e0205e74c72282005b72c8d0dc6740d25f3
brunoeufrasio <bruno10tere@gmail.com>
2021-12-19 20:33:10 -0300

    inicialização de projeto

---
+++ src/test/java/br/com/cardapiodigital/CardapioDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cardapiodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
